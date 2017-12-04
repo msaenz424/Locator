f833d6cc10786ef68d1d6b164a227ce063a546a6
Miguel Saenz <msaenz424@gmail.com>
2017-12-03 19:48:36 -0800

    feat: added runtime permission request for location

--- app/src/main/java/com/migcavero/pushcartlocator/seller/view/MainActivity.kt
+++ app/src/main/java/com/migcavero/pushcartlocator/seller/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.migcavero.pushcartlocator.seller.view
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import com.migcavero.pushcartlocator.seller.BuildConfig
@@ -10,6 +13,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val PERMISSION_REQUEST_CODE = 101
     private val RC_SIGN_IN = 123
 
     private lateinit var mFirebaseAuth: FirebaseAuth
@@ -24,7 +28,7 @@ class MainActivity : AppCompatActivity() {
             val user = firebaseAuth.currentUser
             if (user != null) {
                 // already signed in
-
+                requestPermission()
             } else {
                 // not signed in
                 displayLoginMethods()
@@ -43,6 +47,16 @@ class MainActivity : AppCompatActivity() {
         mFirebaseAuth.removeAuthStateListener(mAuthStateListener)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    finish()
+                }
+            }
+        }
+    }
+
     /**
      * Displays the Firebase's default UI login screen
      */
@@ -56,6 +70,18 @@ class MainActivity : AppCompatActivity() {
                                         AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()))
                         .build(),
                 RC_SIGN_IN)
+    }
+
+    /**
+     * Ask the user for location permission if it wasn't granted
+     */
+    private fun requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+            }
+        }
     }
 
 }
